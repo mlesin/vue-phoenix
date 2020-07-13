4e1394cb2a53f166156d25a0ded1639f7fda337b
Mikhail Lesin <mlesin@mrpost.ru>
2020-07-13 18:13:53 +0300

    build related changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
-import typescript from 'rollup-plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/index.umd.js',
-    format: 'umd',
-    name: 'VuePhoenix',
+    file: pkg.main,
+    format: 'cjs',
     exports: 'named',
+    sourcemap: true,
     globals: {
       vue: 'Vue',
       'vue-class-component': 'VueClassComponent',
       phoenix: 'phoenix'
     }
   },
-  external: ['vue', 'vue-class-component', 'phoenix'],
-  plugins: [typescript()]
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    typescript({
+      tsconfig: 'src/tsconfig.json'
+    })
+  ]
 }
