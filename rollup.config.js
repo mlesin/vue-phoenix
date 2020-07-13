28625db725f67e9ad7ead103d377eeb89908e1a8
Mikhail Lesin <mlesin@mrpost.ru>
2020-07-13 15:46:34 +0300

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
+import typescript from 'rollup-plugin-typescript'
+
 export default {
-  input: 'dist/index.js',
-  name: 'VuePhoenix',
+  input: 'src/index.ts',
   output: {
     file: 'dist/index.umd.js',
-    format: 'umd'
+    format: 'umd',
+    name: 'VuePhoenix',
+    exports: 'named',
+    globals: {
+      vue: 'Vue',
+      'vue-class-component': 'VueClassComponent',
+      phoenix: 'phoenix'
+    }
   },
-  external: [
-    'vue', 'vue-class-component', 'phoenix'
-  ],
-  exports: 'named',
-  name: 'vue-phoenix',
-  globals: {
-    'vue': 'Vue',
-    'vue-class-component': 'VueClassComponent',
-    'phoenix': 'phoenix'
-  }
+  external: ['vue', 'vue-class-component', 'phoenix'],
+  plugins: [typescript()]
 }
